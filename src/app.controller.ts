b4b2efb2b277c2cddfb52483d8ca2204dc243abc
IsthisLee <rjsgmldnwn@gmail.com>
2023-08-07 02:33:27 +0900

    feat: Add healthcheck api for ALB Target Group Check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Head } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Head('/healthcheck')
+  getHealthCheck2(): string {
+    return this.appService.getHealthCheck();
   }
 }
